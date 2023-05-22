7acde7a3adc4eccdd8a10c8e0adef29f6d926386
SHREYAS\shreyas.ir <shreyas.ir@outlook.com>
2023-05-23 03:12:17 +0530

    Add project files.

---
+++ LearningCSharp.Library/Pagination/Pagination.cs
@@ -0,0 +1,24 @@
+﻿namespace LearningCSharp.Library.Pagination
+{
+    internal static class Pagination
+    {
+        // Get data of paged value except for first and last page
+        public static IQueryable<T> Page<T>(this IQueryable<T> data, int pageIndex, int pageSize)
+            => data.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+
+        //Get data of first page
+        public static IQueryable<T> FirstPage<T>(this IQueryable<T> data, int pageSize)
+            => data.Take(pageSize);
+
+        //Get data of last page
+        public static IQueryable<T> LastPage<T>(this IQueryable<T> data, int pageSize)
+            => data.Skip(((data.Count() / pageSize) - 1) * pageSize).Take(pageSize);
+
+        //Get Total count of page
+        public static int CountOfPages<T>(this IQueryable<T> data, int pageSize)
+        {
+            var total = data.Count();
+            return (total / pageSize) + ((total % pageSize) > 0 ? 1 : 0);
+        }
+    }
+}
